33febf1ce27ff00dc6b2792ccc702b579bfb3029
Александр Ломоносов <333688@niuitmo.ru>
2026-06-14 21:29:15 +0300

    [application] Add Spring Boot application skeleton. MPE#mvp

---
+++ src/main/java/ru/fcref/system/FcRefSystemApplication.java
@@ -0,0 +1,12 @@
+package ru.fcref.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcRefSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcRefSystemApplication.class, args);
+    }
+}
